51b2a8b537a019d8fb691f4358b9c73844ed7beb
dongmao zhang <dmzhang@suse.com>
2014-06-11 18:21:26 +0800

    add web

--- testweb/web.go
+++ testweb/web.go
@@ -1,16 +1,33 @@
 package main
 
 import "github.com/codegangsta/martini"
+import "github.com/codegangsta/martini-contrib/render"
+import "database/sql"
+import _ "github.com/mattn/go-sqlite3"
 
 func main() {
+    /* init database */
+    db, err := sql.Open("sqlite3", "/tmp/post_db.bin")
+    if err != nil {
+	    checkErr(err, "open database failed")
+	    return
+    }
+    defer db.Close()
 
-    server := martini.Classic()
-    server.Get("/", func() string {
 
-        return "<h1>Hello, world!</h1>"
+    m := martini.Classic()
+    m.Use(render.Renderer())
 
+
+    m.Get("/", func(r render.Render){
+	    pm := getListofPhysicalMachine(db)
+	    r.HTML(200, "list" , pm)
     })
 
-    server.Run()
+    m.Get("/hello/:name", func(params martini.Params, r render.Render) string {
+	      return "Hello " + params["name"]
+    })
 
+
+    m.Run()
 }
